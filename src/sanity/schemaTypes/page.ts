f9cabad9ce1002dbbda7654737fc2abfccd47b7a
veeso <christian.visintin@veeso.dev>
2025-06-16 11:33:48 +0200

    fix: page

--- src/sanity/schemaTypes/page.ts
+++ src/sanity/schemaTypes/page.ts
@@ -1,5 +1,5 @@
 import { defineField, defineType } from "sanity";
-import { sectionText } from "./sectionText";
+import { sectionFaq } from "./sectionFaq";
 import React from "react";
 import { seo } from "./seo";
 import { EarthGlobeIcon } from "@sanity/icons";
@@ -25,110 +25,27 @@ export const pageType = defineType({
         maxLength: 96,
       },
     }),
-    {
-      name: "language",
-      type: "string",
-      readOnly: true,
-      hidden: true,
-    },
-
-    {
-      type: "object",
-      name: "heroSection",
-
-      fields: [
-        defineField({
-          name: "headline",
-          type: "string",
-          description: "Main title for the section (recommended).",
-        }),
-        {
-          name: "text",
-          type: "blockContent",
-          hidden: ({ document }) => document?.template === "news",
-        },
-        {
-          name: "ascii",
-          title: "ASCII Image",
-          type: "code",
-          options: {
-            language: "plaintext",
-            languageAlternatives: [],
-          },
-          hidden: ({ document }) => document?.template === "news",
-        },
-        defineField({
-          name: "links",
-          hidden: ({ document }) => document?.template !== "default",
-          type: "array",
-          description:
-            "One or more buttons to redirect other urls, pages or files (optional).",
-          of: [
-            defineField({
-              name: "link",
-              type: "link",
-            }),
-          ],
-        }),
-      ],
-    },
     defineField({
       name: "sections",
       type: "array",
-      of: [sectionText],
+      of: [sectionFaq],
       hidden: ({ parent }) => parent?.template !== "default",
     }),
-    {
-      name: "template",
-      type: "string",
-      options: {
-        list: [
-          {
-            title: "Default Page",
-            value: "default",
-          },
-          {
-            title: "Contact Page",
-            value: "contact",
-          },
-          {
-            title: "News Page",
-            value: "news",
-          },
-        ],
-      },
-      validation: (rule) => rule.required(),
-    },
     seo,
   ],
   preview: {
     select: {
       title: "title",
-      language: "language",
     },
-    prepare({ title, language }) {
-      // Directly set the emoji based on language
-      const flagEmoji =
-        language === "en"
-          ? "🇬🇧"
-          : language === "it"
-            ? "🇮🇹"
-            : language === "fr"
-              ? "🇫🇷"
-              : language === "de"
-                ? "🇮🇩"
-                : "🌐";
-
+    prepare({ title }) {
       // Render flagEmoji directly as media using a text-based representation
       return {
         title,
         subtitle: "",
         media: () =>
-          React.createElement(
-            "span",
-            { style: { fontSize: "1rem", display: "block" } },
-            flagEmoji,
-          ),
+          React.createElement("span", {
+            style: { fontSize: "1rem", display: "block" },
+          }),
       };
     },
   },
