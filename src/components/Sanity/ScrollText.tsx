5992e5a68d64e9efd71f23ee638318f7a119fe65
Giorgia <giorgiabagnasco@gmail.com>
2025-06-16 14:37:38 +0200

    Style: Marquee

--- src/components/Sanity/ScrollText.tsx
+++ src/components/Sanity/ScrollText.tsx
@@ -10,11 +10,11 @@ const ScrollText = ({ text }: Props) => {
   const repeatedText = Array(10).fill(text);
   return (
     <div className="relative flex items-center overflow-x-hidden bg-white/10">
-      <div className="animate-marquee font-halo flex w-full items-center gap-2 py-1 text-xl leading-[0.95] whitespace-nowrap uppercase sm:text-[70px]">
+      <div className="animate-marquee font-halo flex w-full items-center gap-2 py-1 text-5xl leading-[0.95] whitespace-nowrap uppercase md:text-6xl lg:text-7xl">
         {repeatedText.map((line, index) => (
           <React.Fragment key={index}>
-            <span className="pt-2">{line}</span>
-            <span className="hidden sm:inline-block">
+            <span className="pt-[.2em]">{line}</span>
+            <span className="mx-4 hidden sm:inline-block">
               <Globe height={40} width={70} />
             </span>
             <span className="inline-block sm:hidden">
