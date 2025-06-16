718ab600e99e8c76f3ee7a20281810ca47dc6211
veeso <christian.visintin@veeso.dev>
2025-06-16 11:07:45 +0200

    feat: faq page

--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -3,11 +3,13 @@ import { blockContentType } from "./blockContent";
 import { linkType } from "./link";
 import { postType } from "./post";
 import { homepageType } from "./homepage";
+import { faqType } from "./faq";
 import { settingsType } from "./settings";
 
 export const schemaTypes = [
   pageType,
   blockContentType,
+  faqType,
   linkType,
   postType,
   homepageType,
